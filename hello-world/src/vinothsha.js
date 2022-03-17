03a4776ed2bbc7857fd137eee031f1b47cbf836a
vinothsha <kvinothsha@gmail.com>
2022-03-17 11:05:36 +0530

    initial

---
+++ hello-world/src/vinothsha.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const VinothSha = () => {
+  
+  function shaHello() {
+    alert('Hello,Hussain welcome to React DeployMent,we want wadagans!');
+  }
+  
+  return (
+    <button onClick={shaHello}>Hussain clickIt!</button>
+  );
+};
+
+export default VinothSha;
